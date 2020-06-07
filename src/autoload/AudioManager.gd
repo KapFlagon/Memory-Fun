142235af8b4563077a81808c9abe4bf57b5022b6
Jack Grimes <flangojack@hotmail.com>
2020-06-07 21:15:23 +0100

    refactored code and design of audio manager. Now sliders respond in real time, and audio doesn't clip.

--- src/autoload/AudioManager.gd
+++ src/autoload/AudioManager.gd
@@ -1,15 +1,16 @@
 extends Node
 
 var music_on := true setget set_music_on, get_music_on
-var music_vol := 100 setget set_music_vol, get_music_vol
+var music_vol := 0 setget set_music_vol, get_music_vol
 var sfx_on := true setget set_sfx_on, get_sfx_on
-var sfx_vol := 100 setget set_sfx_vol, get_sfx_vol
+var sfx_vol := 0 setget set_sfx_vol, get_sfx_vol
 var initialized := false
 var random = RandomNumberGenerator.new()
 
 
 func set_music_on(val: bool) -> void:
 	music_on = val
+	check_music()
 
 
 func get_music_on():
@@ -18,6 +19,7 @@ func get_music_on():
 
 func set_music_vol(val) -> void:
 	music_vol = val 
+	get_node("Music").set_volume_db(music_vol)
 
 
 func get_music_vol():
@@ -34,6 +36,7 @@ func get_sfx_on():
 
 func set_sfx_vol(val) -> void:
 	sfx_vol = val
+	play_rand_sfx()
 
 
 func get_sfx_vol():
@@ -42,8 +45,7 @@ func get_sfx_vol():
 
 func check_music() -> void:
 	if get_music_on():
-		var temp_vol = lin_to_db(get_music_vol())
-		get_node("Music").set_volume_db(temp_vol)
+		get_node("Music").set_volume_db(get_music_vol())
 		var current_position = get_node("Music").is_playing()
 		if current_position == false:
 			get_node("Music").play(0)
@@ -51,29 +53,22 @@ func check_music() -> void:
 		get_node("Music").stop()
 
 
-func lin_to_db(val):
-	var output = val / 100.0 
-	output = output * 80
-	output = output - 80
-	return output
-
-
 func play_rand_sfx() -> void:
 	var rand_sound = random.randi_range(0, 2)
 	var sfx_vol = AudioManager.get_sfx_vol()
 	match rand_sound:
 		0: 
-			get_node("SFX_01").set_volume_db(lin_to_db(AudioManager.get_sfx_vol()))
+			get_node("SFX_01").set_volume_db(get_sfx_vol())
 			get_node("SFX_01").play()
 		1: 
-			get_node("SFX_02").set_volume_db(lin_to_db(AudioManager.get_sfx_vol()))
+			get_node("SFX_02").set_volume_db(get_sfx_vol())
 			get_node("SFX_02").play()
 		2: 
-			get_node("SFX_03").set_volume_db(lin_to_db(AudioManager.get_sfx_vol()))
+			get_node("SFX_03").set_volume_db(get_sfx_vol())
 			get_node("SFX_03").play()
 
 
 func play_victory_sound() -> void: 
 	if AudioManager.get_sfx_on():
-		get_node("VictorySound").set_volume_db(lin_to_db(AudioManager.get_sfx_vol()))
+		get_node("VictorySound").set_volume_db(get_sfx_vol())
 		get_node("VictorySound").play()
--- src/screens/OptionsScreen.gd
+++ src/screens/OptionsScreen.gd
@@ -1,18 +1,30 @@
 extends Control
 
+var original_background_colour
+var original_sfx_on
+var original_sfx_vol
+var original_music_on 
+var original_music_vol
+var new_background_colour
+var new_sfx_on
+var new_sfx_vol
+var new_music_on 
+var new_music_vol
+
 
 func _ready() -> void:
 	get_node("ColorRect").set_frame_color(PlayerData.get_background_colour())
-	get_player_data()
+	get_config_data()
 
 
 func _on_SubmitOptionsButton_button_up() -> void:
 	AudioManager.play_rand_sfx()
-	set_player_data()
+	set_config_data()
 	change_screen()
 
 
 func _on_CancelButton_button_up() -> void:
+	use_original_data()
 	AudioManager.play_rand_sfx()
 	change_screen()
 
@@ -21,17 +33,49 @@ func change_screen() -> void:
 	get_tree().change_scene("res://src/screens/StartScreen.tscn")
 
 
-func get_player_data() -> void:
-	get_node("VBox_Options/GridContainer/ColorPickerButton").set_pick_color(PlayerData.get_background_colour())
-	get_node("VBox_Options/GridContainer/SfxOnChckbx").pressed = AudioManager.get_sfx_on()
-	get_node("VBox_Options/GridContainer/SfxVol_Slider").set_value(AudioManager.get_sfx_vol())
-	get_node("VBox_Options/GridContainer/MusicOnChckbx").pressed = AudioManager.get_music_on()
-	get_node("VBox_Options/GridContainer/MusicVol_Slider").set_value(AudioManager.get_music_vol())
+func get_config_data() -> void:
+	original_background_colour = PlayerData.get_background_colour()
+	original_sfx_on = AudioManager.get_sfx_on()
+	original_sfx_vol = AudioManager.get_sfx_vol()
+	original_music_on = AudioManager.get_music_on()
+	original_music_vol = AudioManager.get_music_vol()
+	use_original_data()
 
-func set_player_data() -> void: 
+func set_config_data() -> void: 
 	PlayerData.set_background_colour(get_node("VBox_Options/GridContainer/ColorPickerButton").get_pick_color())
 	AudioManager.set_sfx_on(get_node("VBox_Options/GridContainer/SfxOnChckbx").pressed)
 	AudioManager.set_sfx_vol(get_node("VBox_Options/GridContainer/SfxVol_Slider").get_value())
 	AudioManager.set_music_on(get_node("VBox_Options/GridContainer/MusicOnChckbx").pressed)
 	AudioManager.set_music_vol(get_node("VBox_Options/GridContainer/MusicVol_Slider").get_value())
 	AudioManager.check_music()
+
+
+func use_original_data() -> void:
+	get_node("VBox_Options/GridContainer/ColorPickerButton").set_pick_color(original_background_colour)
+	get_node("VBox_Options/GridContainer/SfxOnChckbx").pressed = original_sfx_on
+	get_node("VBox_Options/GridContainer/SfxVol_Slider").set_value(original_sfx_vol)
+	get_node("VBox_Options/GridContainer/MusicOnChckbx").pressed = original_music_on
+	get_node("VBox_Options/GridContainer/MusicVol_Slider").set_value(original_music_vol)
+
+
+func _on_ColorPickerButton_color_changed(color: Color) -> void:
+	new_background_colour = get_node("VBox_Options/GridContainer/ColorPickerButton").get_pick_color()
+	get_node("ColorRect").set_frame_color(color)
+
+
+func _on_SfxOnChckbx_toggled(button_pressed: bool) -> void:
+	new_sfx_on = button_pressed
+	AudioManager.set_sfx_on(new_sfx_on)
+
+
+func _on_SfxVol_Slider_value_changed(value: float) -> void:
+	AudioManager.set_sfx_vol(value)
+
+
+func _on_MusicOnChckbx_toggled(button_pressed: bool) -> void:
+	new_music_on = button_pressed
+	AudioManager.set_music_on(new_music_on)
+
+
+func _on_MusicVol_Slider_value_changed(value: float) -> void:
+	AudioManager.set_music_vol(value)
