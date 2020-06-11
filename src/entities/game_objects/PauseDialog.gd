81d35572179319425350103b3a147b531d323f30
Jack Grimes <flangojack@hotmail.com>
2020-06-12 00:02:08 +0100

    Removed bug in gameplay timer, where timer continued even while game was paused.

--- src/entities/game_objects/PauseDialog.gd
+++ src/entities/game_objects/PauseDialog.gd
@@ -4,12 +4,12 @@ var pause_start_time
 var pause_end_time
 var total_pause_time setget , get_total_pause_time
 
-signal unpaused(total_time)
+signal unpaused(elapsed_pause_time)
 signal pause_options_submitted
 
 
 func _ready() -> void:
-	pause_start_time = OS.get_unix_time()
+	pass
 
 
 func _process(delta): 
@@ -26,6 +26,7 @@ func get_total_pause_time():
 
 func _on_PauseDialog_popup_hide() -> void:
 	total_pause_time = pause_end_time - pause_start_time
+	print("total_pause_time: " + str(total_pause_time))
 	emit_signal("unpaused", get_total_pause_time())
 
 
@@ -42,3 +43,7 @@ func _on_OptionsButton_options_submitted() -> void:
 func _on_MainMenuButton_button_up() -> void:
 	get_tree().paused = false
 	get_tree().change_scene("res://src/screens/StartScreen.tscn")
+
+
+func _on_PauseDialog_about_to_show() -> void:
+	pause_start_time = OS.get_unix_time()
--- src/entities/ui/Hud.gd
+++ src/entities/ui/Hud.gd
@@ -1,9 +1,10 @@
 extends Control
 
+
 var start_time
-var time_now setget set_time_now
-var end_time
+var elapsed_time 
 var str_elapsed_time setget , get_str_elapsed_time
+
 
 func _ready() -> void:
 	start_time = OS.get_unix_time()
@@ -11,10 +12,10 @@ func _ready() -> void:
 
 func _process(delta): 
 	update_time_label()
-	
-	
+
+
 func update_time_label():
-	time_now = OS.get_unix_time()
+	var time_now = OS.get_unix_time()
 	var elapsed_time = time_now - start_time
 	var minutes = elapsed_time / 60
 	var seconds = elapsed_time % 60
@@ -27,7 +28,5 @@ func get_str_elapsed_time():
 	return str_elapsed_time
 
 
-func set_time_now(new_time) -> void:
-	var string_array = new_time.split(":", false)
-	var a = int(string_array[0]) * 60
-	var b = a + int(string_array[1])
+func _on_PauseDialog_unpaused(elapsed_pause_time) -> void:
+	start_time = start_time + elapsed_pause_time
