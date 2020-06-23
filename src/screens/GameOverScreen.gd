d6ef8cf830df16ebff1325d14e3caf264af3869d
Jack Grimes <flangojack@hotmail.com>
2020-06-23 10:22:00 +0100

    Added a pop-up for name entry after game completed. Preparing for high score saving functionality.

--- src/screens/GameOverScreen.gd
+++ src/screens/GameOverScreen.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	AudioManager.play_victory_sound()
 	get_node("ColorRect").set_frame_color(PlayerData.get_background_colour())
 	$VBox/HBoxContainer/StatText.set_text(PlayerData.get_game_time())
+	get_node("Popup_NameEntry").popup()
 
 
 func _on_PlayAgainButton_button_up() -> void:
@@ -23,3 +24,9 @@ func _on_OptionsButton_options_submitted() -> void:
 
 func _on_MainMenuButton_button_up() -> void:
 	get_tree().change_scene("res://src/screens/StartScreen.tscn")
+
+
+func _on_SubmitButton_button_up() -> void:
+	# Save score
+	get_node("Popup_NameEntry").hide()
+	pass # Replace with function body.
