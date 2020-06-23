e965e0c7c2fcfe528271d9b041c3d75fda5fc4ea
Jack Grimes <flangojack@hotmail.com>
2020-06-23 10:00:29 +0100

    Finished 'How To Play' instructions.

---
+++ src/entities/scenelessScripts/ScoreManager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+
+# three data structures, each containing a grid, each containing five best times. 
--- src/screens/HowToPlayScreen.gd
+++ src/screens/HowToPlayScreen.gd
@@ -47,6 +47,8 @@ func update_display() -> void:
 	var img_2 = null
 	var text_3
 	var img_3 = null
+
+
 	# Page 1
 	if current_page == 1:
 		var game_start_text = "Select a difficulty level and a grid size to begin."
@@ -55,42 +57,40 @@ func update_display() -> void:
 		var difficulty_text = "Higher difficulty levels introduce more shapes and colours."
 		text_1 = game_start_text
 		text_2 = goal_text
-		img_2 = goal_img
 		text_3 = difficulty_text
+		get_node("Img_1").show()
+		get_node("Img_2").hide()
+		get_node("Img_3").hide()
+		get_node("Img_4").hide()
+		get_node("Img_5").hide()
 	# Page 2
 	if current_page == 2:
-		var controls_directions_text = "Use the 'WASD' keys, the arrow keys, or left-mouse clicks to move the selector icon."
-		var controls_input_text = "Use the Space key or Enter key to flip a card. The left-mouse click also flips card."
-		var failed_match_text = "If you flip two cards that don't match, they will turn face down again."
+		var controls_directions_text = "Use the 'WASD' keys, the arrow keys, \nor left-mouse clicks to move the selector icon."
+		var controls_input_text = "Use the Space key or Enter key to flip a card. \nThe left-mouse click also flips card."
+		
 		var failed_match_img = load("res://assets/images/how_to/HowTo_02.png")
 		text_1 = controls_directions_text
 		text_2 = controls_input_text
-		text_3 = failed_match_text
-		img_3 = failed_match_img
+		text_3 = ""
+		get_node("Img_1").hide()
+		get_node("Img_2").show()
+		get_node("Img_3").hide()
+		get_node("Img_4").hide()
+		get_node("Img_5").hide()
 	# Page 3
 	if current_page == 3: 
-		var colourblind_off_text = "When colourblind mode is disabled, cards have a shape and a colour which must be matched."
-		var colourblind_off_img = load("res://assets/images/how_to/HowTo_03.png")
-		var colourblind_on_text = "When colourblind mode is enabled, cards have no colours and instead use additional background shapes."
-		var colourblind_on_img = load("res://assets/images/how_to/HowTo_04.png")
-		var timer_text = "You can see your time at the bottom of the screen. It will be your score."
-		var timer_img = load("res://assets/images/how_to/HowTo_05.png")
-		text_1 = colourblind_off_text
-		img_1 = colourblind_off_img
-		text_2 = colourblind_on_text
-		img_2 = colourblind_on_img
+		var failed_match_text = "If you flip two cards that don't match, \nthey will turn face down again.\n"
+		var colourblind_text = "Colourblind mode disabled: \nCards have a shape and a colour which must be matched.\n\n\nColourblind mode enabled: \nWhite cards use additional background shapes.\n\n"
+		var timer_text = "You can see your time at the bottom of the screen. \nIt will be your score."
+		text_1 = failed_match_text
+		text_2 = colourblind_text
 		text_3 = timer_text
-		img_3 = timer_img
+		get_node("Img_1").hide()
+		get_node("Img_2").hide()
+		get_node("Img_3").show()
+		get_node("Img_4").show()
+		get_node("Img_5").show()
 	
-	get_node("VBoxContainer/HBox_1/Txt_1").set_text(text_1)
-	get_node("VBoxContainer/HBox_2/Txt_2").set_text(text_2)
-	get_node("VBoxContainer/HBox_3/Txt_3").set_text(text_3)
-	
-	if img_1 != null:
-		pass
-		
-	if img_2 != null: 
-		pass
-	if img_3 != null: 
-		pass
-	
+	get_node("VBoxContainer/Txt_1").set_text(text_1)
+	get_node("VBoxContainer/Txt_2").set_text(text_2)
+	get_node("VBoxContainer/Txt_3").set_text(text_3)
