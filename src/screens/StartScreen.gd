968e8f86ab92968a51cec2f328bd4c5aaa4d0df4
Jack Grimes <flangojack@hotmail.com>
2020-06-28 22:50:16 +0100

    Updated code in Start screen to use internationalization texts.

--- src/screens/StartScreen.gd
+++ src/screens/StartScreen.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_node("ColorRect").set_frame_color(PlayerData.get_background_colour())
+	ready_texts()
 	if AudioManager.initialized == false:
 		AudioManager.check_music()
 		AudioManager.initialized = true
@@ -33,3 +34,12 @@ func _on_HighScoreButton_button_up() -> void:
 func _on_OptionsButton_popup_hidden() -> void:
 	get_node("ColorRect").set_frame_color(PlayerData.get_background_colour())
 	get_node("VBox_MainMenu/HBoxContainer/ColourBlindCheckBox").pressed = PlayerData.get_colour_blind_mode_on()
+
+
+func ready_texts() -> void: 
+	get_node("VBox_MainMenu/StartButton").set_text(tr("START"))
+	get_node("VBox_MainMenu/OptionsButton").set_text(tr("OPTIONS"))
+	get_node("VBox_MainMenu/HowToPlayButton").set_text(tr("HOW_TO_PLAY"))
+	get_node("VBox_MainMenu/HighScoreButton").set_text(tr("HIGH_SCORES"))
+	get_node("VBox_MainMenu/CreditsButton").set_text(tr("CREDITS"))
+	get_node("VBox_MainMenu/HBoxContainer/ColourBlindCheckBox").set_text(tr("COLOURBLIND_MODE"))
