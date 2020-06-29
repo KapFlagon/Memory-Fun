d5e57a35f6d7c379b7788ae3ed172405133f7a64
Jack Grimes <flangojack@hotmail.com>
2020-06-29 22:57:56 +0100

    Updated Difficulty screen to use internationalization data.

--- src/screens/DifficultySelectionScreen.gd
+++ src/screens/DifficultySelectionScreen.gd
@@ -4,14 +4,10 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_node("ColorRect").set_frame_color(PlayerData.get_background_colour())
+	ready_texts()
 	for button in get_tree().get_nodes_in_group("DifficultyGroup"):
 		button.connect("button_up", self, "_difficulty_button_pressed", [button])
 	set_tooltip_texts()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
 
 
 func _on_BackButton_button_up() -> void:
@@ -36,10 +32,24 @@ func _difficulty_button_pressed(button):
 
 func set_tooltip_texts() -> void:
 	if PlayerData.get_colour_blind_mode_on():
-		get_node("VBox_DifficultySelection/EasyDifButton").set_tooltip("3 Shapes, 1 Background Icon")
-		get_node("VBox_DifficultySelection/MedDifButton").set_tooltip("5 Shapes, 3 Background Icons")
-		get_node("VBox_DifficultySelection/HardDifButton").set_tooltip("5 Shapes, 7 Background Icons")
+#		get_node("VBox_DifficultySelection/EasyDifButton").set_tooltip("3 Shapes, 1 Background Icon")
+#		get_node("VBox_DifficultySelection/MedDifButton").set_tooltip("5 Shapes, 3 Background Icons")
+#		get_node("VBox_DifficultySelection/HardDifButton").set_tooltip("5 Shapes, 7 Background Icons")
+		get_node("VBox_DifficultySelection/EasyDifButton").set_tooltip(tr("DIFF_EASY_HINT_CB"))
+		get_node("VBox_DifficultySelection/MedDifButton").set_tooltip(tr("DIFF_MED_HINT_CB"))
+		get_node("VBox_DifficultySelection/HardDifButton").set_tooltip(tr("DIFF_HARD_HINT_CB"))
 	else:
-		get_node("VBox_DifficultySelection/EasyDifButton").set_tooltip("3 Shapes, 1 Colour")
-		get_node("VBox_DifficultySelection/MedDifButton").set_tooltip("5 Shapes, 3 Colours")
-		get_node("VBox_DifficultySelection/HardDifButton").set_tooltip("5 Shapes, 7 Colours")
+#		get_node("VBox_DifficultySelection/EasyDifButton").set_tooltip("3 Shapes, 1 Colour")
+#		get_node("VBox_DifficultySelection/MedDifButton").set_tooltip("5 Shapes, 3 Colours")
+#		get_node("VBox_DifficultySelection/HardDifButton").set_tooltip("5 Shapes, 7 Colours")
+		get_node("VBox_DifficultySelection/EasyDifButton").set_tooltip(tr("DIFF_EASY_HINT_COL"))
+		get_node("VBox_DifficultySelection/MedDifButton").set_tooltip(tr("DIFF_MED_HINT_COL"))
+		get_node("VBox_DifficultySelection/HardDifButton").set_tooltip(tr("DIFF_HARD_HINT_COL"))
+
+
+func ready_texts() -> void:
+	$VBox_DifficultySelection/DifficultyPrompt.set_text(tr("DIFF_PROMPT")+":")
+	$VBox_DifficultySelection/EasyDifButton.set_text(tr("DIFF_EASY"))
+	$VBox_DifficultySelection/MedDifButton.set_text(tr("DIFF_MED"))
+	$VBox_DifficultySelection/HardDifButton.set_text(tr("DIFF_HARD"))
+	$VBox_DifficultySelection/BackButton.set_text(tr("BACK"))
